5566e0e6ec8a20b5a884a4f25c238a3e9ea861e8
Abhinaw Tiwari <abhinawtiwari@Abhinaws-MacBook-Pro.local>
2022-12-02 21:24:55 +0000

    updated notebook for plots

--- test.py
+++ test.py
@@ -1,22 +1,8 @@
-Lasso:
-For q = 1
-Below plot for q =  1 , C =  10 , intercept:  0.34388261320849267  and coef:  
-[ 0.00000000e+00 -1.89394390e-01 -7.29221789e-17  8.02924708e+00
-  1.62490685e+00  9.74490512e-01 -0.00000000e+00 -0.00000000e+00
-  0.00000000e+00  0.00000000e+00]
-Below plot for q =  1 , C =  100 , intercept:  0.27424591285740973  and coef:  [ 0.00000000e+00 -1.94393773e-01 -2.77649101e-17  8.02996035e+00
-  1.64543377e+00  2.53287697e+00 -0.00000000e+00 -0.00000000e+00
-  8.08648301e-01  0.00000000e+00]
-
-  ['team_name', 'venue_name', 'ball_no', 'run', 'is_wide', 'is_noBall', 'Wicket', 'is_legBye', 'isBye']
-
-ball_no and run for c = 1
-'ball_no', 'is_wide', 'run' for c = 10
-'ball_no', 'run', 'is_wide' is_legBye for c = 100
-
-refer excel for q = 2
-
-Ridge:
-Ridge regression also confirms that 'ball_no', 'run', 'is_wide' is_legBye for c = 100
-
-refer excel for q = 2
+plt.figure(figsize=(9, 5))
+plt.scatter(test_df_short['ball_no'] ,test_df_short['totalScore'],color = 'blue', marker = '+')
+plt.scatter(test_df_short['ball_no'] ,y_pred_final ,color = 'red', marker = '+')
+plt.rc('font', size=10)
+plt.ylabel ('Total Score till that Over')
+plt.xlabel ('Ball no(Overs)')
+plt.legend(['target','Prediction'],loc='lower right')
+plt.title("Ridge Regression: Predicition on test data (Total Score Vs Overs(Ball no))")
